8154826cd337223a8a940448266400774c18dce6
kislaychauhan <93432129+kislaychauhan@users.noreply.github.com>
2023-06-05 11:34:43 +0530

    Add files via upload

---
+++ Vihiti Theatre Club UI/vihiti.js
@@ -0,0 +1,28 @@
+const slides = document.querySelectorAll(".slide")
+var counter = 0;
+
+slides.forEach(
+    (slide, index) => {
+        slide.style.left = '$(index * 100)%'
+    }
+)
+
+const goPrev = () => {
+    counter--
+    alert()
+    slideImage()
+}
+const goNext = () => {
+    counter++
+    alert()
+    slideImage()
+}
+
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = 'translateX(-${counter*100}%)'
+        }
+    )
+}
